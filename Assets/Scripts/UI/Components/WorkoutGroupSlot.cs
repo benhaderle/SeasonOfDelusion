28aaa838cb85c8cde57ac72eeef8d1df20312f72
Ben Haderle <benhaderle@gmail.com>
2025-01-09 19:13:12 -0500

    group slot prefab set up more thoroughly

---
+++ Assets/Scripts/UI/Components/WorkoutGroupSlot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WorkoutGroupSlot : MonoBehaviour
+{
+    [SerializeField] private RectTransform cardParent;
+    [SerializeField] private Image selectionOutline;
+    [SerializeField] private TextMeshProUGUI intensityText;
+}
