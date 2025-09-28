f65c996c6a2a7499a4a3a2ce3971e93cd24e3cf1
Ben Haderle <benhaderle@gmail.com>
2025-09-27 21:18:32 -0400

    build stuff

---
+++ post_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Uploading IPA to App Store Connect..."
+KEY_WITH_NEWLINES=$(echo $CONNECT_API_KEY | jq '.private_key |= sub(" (?!PRIVATE|KEY)"; "\n"; "g")' -c -j)
+echo $KEY_WITH_NEWLINES > YOUR_AUTH_KEY.p8
+if [ ! -f YOUR_AUTH_KEY.p8 ]; then
+echo "Key File not found!"
+fi
+# Assuming you have set API_ISSUER_ID environment variable
+if [ -z "$API_ISSUER_ID" ]; then
+echo "Error: API_ISSUER_ID is not set"
+exit 1
+fi
+if xcrun altool --upload-app -type ios -f "$UNITY_PLAYER_PATH" --apiKey YOUR_API_KEY --apiIssuer "$API_ISSUER_ID"; then
+echo "Upload IPA to App Store Connect finished with success"
+else
+echo "Upload IPA to App Store Connect failed"
+fi
